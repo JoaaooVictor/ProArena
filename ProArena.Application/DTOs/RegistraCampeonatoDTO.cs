330347ce49637d8cc68b46c03ef1823ea24b2681
João Victor <joao.v.l.s1998@outlook.com>
2026-02-09 23:24:06 -0300

    Inclusão dos campos de campeonato no DTO de registro

--- ProArena.Application/DTOs/RegistraCampeonatoDTO.cs
+++ ProArena.Application/DTOs/RegistraCampeonatoDTO.cs
@@ -6,7 +6,8 @@
         public string? Descricao { get; set; }
         public DateTime? DataInicio { get; set; }
         public DateTime? DataFim { get; set; }
-
+        public DateTime? DataInicioInscricao { get; set; }
+        public DateTime? DataFimInscricao { get; set; }
         public List<RegistraEquipeDTO> Equipes { get; set; } = new();
         public List<RegistraPartidaDTO> Partidas { get; set; } = new();
     }
--- ProArena.Domain/Entities/Campeonato.cs
+++ ProArena.Domain/Entities/Campeonato.cs
@@ -5,8 +5,10 @@
         public int CampeonatoId { get; set; }
         public string Nome { get; set; } = string.Empty;
         public string? Descricao { get; set; }
-        public DateTime DataFim { get; set; }
-        public DateTime DataInicio { get; set; }
+        public DateTime? DataInicioInscricao { get; set; }
+        public DateTime? DataFimInscricao { get; set; }
+        public DateTime? DataFim { get; set; }
+        public DateTime? DataInicio { get; set; }
         public virtual List<Equipe> Equipes { get; set; } = new();
         public virtual List<Partida> Partidas { get; set; } = new();
     }
